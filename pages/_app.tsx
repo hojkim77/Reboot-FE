0ab3a998cc589c7b326619b4afca5fd5686e836f
hojkim77 <rlaghwnd4534@gmail.com>
2023-10-06 18:15:48 +0900

    fixedbar 문제 해결

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,22 @@ function RebootAPP({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      {/**
+       * html, body에 overflow-x: hidden을 사용하고
+       * 스크린 css에 position: sticky를 사용하면
+       * sticky가 작동하지 않는다.
+       * 이를 해결하기 위해 overflow-x: clip을 사용한다.
+       * //참고자료: https://www.terluinwebdesign.nl/en/css/position-sticky-not-working-try-overflow-clip-not-overflow-hidden/
+       */}
       <style jsx global>
         {`
           html {
-            overflow-x: hidden;
+            overflow-x: clip;
           }
           body {
             margin: 0;
             width: 100%;
-            overflow-x: hidden;
+            overflow-x: clip;
           }
         `}
       </style>
